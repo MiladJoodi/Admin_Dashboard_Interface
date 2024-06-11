7a422c5a127eff93cfc6e99e6722051152da0ac9
Milad Joodi <winbeta.com@gmail.com>
2024-06-12 01:28:42 +0330

    up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
-import logo from "../img/logo.png"
+import logo from "../img/logo.png";
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = () => {
-    return (
-        <div className="bg-primary dark: bg-slate-700 text-white py-2 px-5 flex justify-between">
-            <Link href="/">
-                <Image src={logo} alt="Logo" width={40}/>
-            </Link>
-        </div>
-    );
-}
+  return (
+    <div className="bg-primary dark: bg-slate-700 text-white py-2 px-5 flex justify-between">
+      <Link href="/">
+        <Image src={logo} alt="Logo" width={40} />
+      </Link>
+
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback className="text-black">BT</AvatarFallback>
+      </Avatar>
+    </div>
+  );
+};
 
 export default Navbar;
