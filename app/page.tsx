46dcc44ba21e257f4a61e82e50033346740471f6
Milad Joodi <winbeta.com@gmail.com>
2024-06-11 22:47:43 +0330

    up

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,13 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl">Dashboard</h1>
-      <Button variant="destructive">Click Me</Button>
+      <Button 
+        variant="destructive" 
+        size='lg'
+        className="text-blue-400"
+      >
+        Click Me
+        </Button>
     </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import logo from "../img/logo.png"
+
+const Navbar = () => {
+    return (
+        <div className="bg-primary dark: bg-slate-700 text-white py-2 px-5 flex justify-between">
+            Navbar
+        </div>
+    );
+}
+
+export default Navbar;
