4c30a23b2a2c671a7c60a9fc16e2a58d340b3150
Milad Joodi <winbeta.com@gmail.com>
2024-06-12 00:37:39 +0330

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import logo from "../img/logo.png"
+import Image from "next/image";
 
 const Navbar = () => {
     return (
         <div className="bg-primary dark: bg-slate-700 text-white py-2 px-5 flex justify-between">
-            Navbar
+            <Link href="/">
+                <Image src={logo} alt="Logo" width={40}/>
+            </Link>
         </div>
     );
 }
