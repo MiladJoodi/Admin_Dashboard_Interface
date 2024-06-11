053697774c00e18213fa13706a9d22c4c4bfdd2a
Milad Joodi <winbeta.com@gmail.com>
2024-06-11 22:14:47 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 // https://www.youtube.com/watch?v=hhudoSMM0yU
 // https://github.com/bradtraversy/traversypress-ui
 export default function Home() {
   return (
-    <h1 className="text-2xl">Dashboard</h1>
+    <>
+      <h1 className="text-2xl">Dashboard</h1>
+      <Button variant="destructive">Click Me</Button>
+    </>
   );
 }
