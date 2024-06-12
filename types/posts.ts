2b96715844304dc24ffceab1ab79bfa6db1ff07c
Milad Joodi <winbeta.com@gmail.com>
2024-06-12 21:31:26 +0330

    up

---
+++ types/posts.ts
@@ -0,0 +1,13 @@
+export interface Post {
+    id: string;
+    title: string;
+    body: string;
+    author: string;
+    comments: PostComment[];
+}
+
+export interface PostComment {
+    id: string;
+    text: string;
+    username: string;
+}
