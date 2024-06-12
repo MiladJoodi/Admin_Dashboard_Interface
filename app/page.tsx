41cf40512a0985850fde4e2b0bd03b34f3848b9a
Milad Joodi <winbeta.com@gmail.com>
2024-06-12 21:21:10 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
 import { Button } from "@/components/ui/button";
-import { Newspaper } from "lucide-react";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 import Image from "next/image";
 // https://www.youtube.com/watch?v=hhudoSMM0yU
 // https://github.com/bradtraversy/traversypress-ui
@@ -9,6 +9,9 @@ export default function Home() {
     <>
       <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
         <DashboardCard title="Posts" count={100} icon={<Newspaper className="text-slate-500" size={72} />} />
+        <DashboardCard title="Categories" count={12} icon={<Folder className="text-slate-500" size={72} />} />
+        <DashboardCard title="User" count={750} icon={<User className="text-slate-500" size={72} />} />
+        <DashboardCard title="Comments" count={1200} icon={<MessageCircle className="text-slate-500" size={72} />} />
       </div>
     </>
   );
