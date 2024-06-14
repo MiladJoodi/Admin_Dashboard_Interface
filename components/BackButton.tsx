04664efcf5a5c8b5d25562e78503b353cce34229
Milad Joodi <winbeta.com@gmail.com>
2024-06-14 21:55:17 +0330

    Create BackButton.tsx

---
+++ components/BackButton.tsx
@@ -0,0 +1,16 @@
+import { ArrowLeft } from "lucide-react";
+
+interface BackButtonProps {
+    text: string;
+    link: string;
+}
+
+const BackButton = ({text, link}: BackButtonProps) => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default BackButton;
