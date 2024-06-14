0ff3876d8cf048577d5641ff38e84b994220e479
Milad Joodi <winbeta.com@gmail.com>
2024-06-14 21:57:51 +0330

    u[

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,9 +1,11 @@
+import BackButton from "@/components/BackButton";
 import PostsTable from "@/components/posts/PostsTable";
 
 
 const PostPage = () => {
     return (
         <>
+        <BackButton text="Go Back" link="/" />
         <PostsTable />
         </>
     );
--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,4 +1,5 @@
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, ArrowLeftCircle } from "lucide-react";
+import Link from "next/link";
 
 interface BackButtonProps {
     text: string;
@@ -7,9 +8,9 @@ interface BackButtonProps {
 
 const BackButton = ({text, link}: BackButtonProps) => {
     return (
-        <div>
-            Enter
-        </div>
+        <Link href={link} className="text-gray-500 hover:underline flex items-center gap-1 font-bold mb-5">
+            <ArrowLeftCircle size={18} />{text}
+        </Link>
     );
 }
 
