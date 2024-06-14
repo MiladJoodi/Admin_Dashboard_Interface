fafaf53903c4d6e59a313673a66c3642413a0406
Milad Joodi <winbeta.com@gmail.com>
2024-06-14 23:42:34 +0330

    Create page.tsx

---
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import BackButton from "@/components/BackButton";
+
+const PostEditPage = () => {
+    return (
+        <>
+            <BackButton text="Back To Posts" link="/posts" />
+        </>
+    );
+}
+
+export default PostEditPage;
