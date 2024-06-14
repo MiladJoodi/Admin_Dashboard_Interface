67a211ef413147708be4a6b38e9b29e94aef5b97
Milad Joodi <winbeta.com@gmail.com>
2024-06-15 00:43:19 +0330

    up

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -1,4 +1,18 @@
+"use client"
+
 import BackButton from "@/components/BackButton";
+import * as z from 'zod';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+  import { Input } from "@/components/ui/input"
 
 const PostEditPage = () => {
     return (
