1963956ad14b97ca707631cf0beaa9c96651ee5b
Milad Joodi <winbeta.com@gmail.com>
2024-06-15 17:21:37 +0330

    Update page.tsx

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -42,7 +42,15 @@ const PostEditPage = ({params}: PostEditPageProps) => {
 
     const post = posts.find((post)=> post.id === params.id);
 
-    console.log(post)
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: post?.title || '',
+            body: post?.body || '',
+            author: post?.author || '',
+            date: post?.date || ''
+        },
+    });
 
     return (
         <>
