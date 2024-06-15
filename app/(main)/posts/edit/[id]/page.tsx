a4be66f3c73c97ce1729511c3b9553c85ad32683
Milad Joodi <winbeta.com@gmail.com>
2024-06-15 17:06:37 +0330

    Update page.tsx

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -13,6 +13,24 @@ import {
     FormMessage,
   } from "@/components/ui/form"
   import { Input } from "@/components/ui/input"
+  import { Textarea } from "@/components/ui/textarea";
+  import { Button } from "@/components/ui/button";
+  import posts from "@/data/posts";
+
+  const formSchema = z.object({
+    title: z.string().min(1, {
+        message: 'Title is required'
+    }),
+    body: z.string().min(1, {
+        message: 'Body is required'
+    }),
+    author: z.string().min(1, {
+        message: 'Author is required'
+    }),
+    date: z.string().min(1, {
+        message: 'Date is required'
+    }),
+  })
 
 const PostEditPage = () => {
     return (
